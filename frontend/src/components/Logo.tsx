52bfa633379d16f03e5531091b29f2706cb05f90
blks <nbblks@gmail.com>
2021-07-16 18:03:01 +0900

    [style] Add styles to login/signup component

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Logo = () => {
+const Logo = (props: any) => {
   return (
     <div className="absolute flex align-center left-0 m-2">
       <img className="" alt="logo"></img>
-      <span className="ml-2 text-2xl">Hustlo</span>
+      <span className={`ml-2 text-${props.textSize} font-krona`}>Hustlo</span>
     </div>
   );
 };
--- frontend/src/container/Auth.tsx
+++ frontend/src/container/Auth.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import Form from "../components/AuthForm";
+import Footer from "../components/Landing/Footer";
 import Logo from "../components/Logo";
 
 const Auth = (props: any) => {
   return (
-    <div className="w-screen h-screen m-0 p-0 bg-gray-lightest">
-      <Logo />
+    <div className="w-screen h-screen m-0 p-0 bg-gray-lightest flex justify-center items-center">
+      <div className="absolute top-20 transform -translate-x-32">
+        <Logo textSize="5xl" />
+      </div>
       <Form type={props.type} />
+      <div className="absolute bottom-4">
+        <Footer />
+      </div>
     </div>
   );
 };
