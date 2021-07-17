fc20061893a30306e3b459fcc85fce51d85a6b7d
blks <nbblks@gmail.com>
2021-07-17 20:01:46 +0900

    [feature] Set up mongoose

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { connect } from "./database/database";
 
 const app = express();
 const PORT = 8000;
+
+connect();
+
 app.get("/", (req, res) => res.send("Express + Typescript server"));
+
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
