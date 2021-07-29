1f64cafd17a599e4ed537863edc2a7fc7cfb91f0
blks <nbblks@gmail.com>
2021-07-29 21:26:00 +0900

    [feature] Add Microsoft oAuth compoent

--- frontend/src/components/Form/GoogleOauthLoginButton.tsx
+++ frontend/src/components/Form/GoogleOauthLoginButton.tsx
@@ -1,13 +1,23 @@
 import { GoogleLogin } from "react-google-login";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import OauthButton from "./OauthLoginButton";
 
 export const GoogleOauthLoginButton = (props: any) => {
   return (
     <GoogleLogin
       clientId={process.env.REACT_APP_GOOGLE_OAUTH_ID!}
-      buttonText="Continue with Google"
+      buttonText=""
+      icon={false}
       onSuccess={props.googleSuccess}
       onFailure={props.googleFailure}
       cookiePolicy={"single_host_origin"}
+      render={({ onClick, disabled }) => (
+        <OauthButton
+          click={onClick}
+          value="Continue with Google"
+          icon={faGoogle}
+        />
+      )}
     />
   );
 };
