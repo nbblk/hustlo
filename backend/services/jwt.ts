826ab19879593702f70cccbacfd1607a03371dd1
blks <nbblks@gmail.com>
2021-12-03 12:04:41 +0900

    [fix] change jwt expire time

--- backend/services/jwt.ts
+++ backend/services/jwt.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateJwt = (email: string) => {
   let secretKey = process.env.JWT_SECRET_KEY;
-  return jwt.sign({ email: email }, secretKey!, { expiresIn: "1800s" });
+  return jwt.sign({ email: email }, secretKey!, { expiresIn: "18000s" });
 };
 
 export const verifyJwt = (token: string) => {
