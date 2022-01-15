515410bee9b450462e6b9b182a85e48a3d9ca0ac
nbblks <nbblks@gmail.com>
2022-01-15 18:21:41 +0900

    fixup! [fix] moved dependencies and updated tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.jsx',
+//    './src/**/*.html',
+//    './src/**/*.jsx',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
