71251102ab13102dd091d6a76349b53f5d940b17
nbblks <nbblks@gmail.com>
2022-01-16 12:08:50 +0900

    [fix] add prefix /api to all routes

--- backend/index.ts
+++ backend/index.ts
@@ -26,11 +26,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/", authRouter);
-app.use("/workspace", workspaceRouter);
-app.use("/board", boardRouter);
-app.use("/list", listRouter);
-app.use("/card", cardRouter);
+app.use("/api/", authRouter);
+app.use("/api/workspace", workspaceRouter);
+app.use("/api/board", boardRouter);
+app.use("/api/list", listRouter);
+app.use("/api/card", cardRouter);
 
 const options =
   env.NODE_ENV === "dev"
